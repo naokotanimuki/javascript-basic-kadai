1c6151c6bfae5fb3279186438df8dd2c1640980c
naokotanimuki <momomomomo6026@gmail.com>
2025-09-01 16:06:22 +0900

    mdn.jsを再提出

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -4,4 +4,4 @@ const date = new Date();
 
 const month = date.getMonth()+1;
 
-console.log(date.getFullYear() +'年'+ month +'月'+ date.getDay()+'日');
+console.log(date.getFullYear() +'年'+ month +'月'+ date.getDate()+'日');
